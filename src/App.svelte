cc6c9d67cc1c204643db88bf990f7e4671d7ee1d
ka-aki <aki0215ka@gmail.com>
2022-08-18 00:37:39 +0900

    add TodoList

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,8 @@
 <script>
   import ToDoInputForm from "./ToDoInputForm.svelte";
+  import ToDoList from "./ToDoList.svelte";
+
+  let nameEntered = false;
 
   let lastName = "田中";
   let firstName = "花子";
@@ -8,14 +11,27 @@
 </script>
 
 <main>
-  <ToDoInputForm userName={fullName} />
+  {#if nameEntered}
+    <h1>タスクリストアプリケーションへようこそ、{fullName}さん!</h1>
+    <ToDoInputForm userName={fullName} />
+    <ToDoList />
+  {:else}
+    <h1>タスクアプリケーションへようこそ！</h1>
+    <h3>名前を入力してください。</h3>
+    <div>
+      <input type="text" placeholder="姓" required />
+    </div>
+    <div>
+      <input type="text" placeholder="名" required />
+    </div>
+  {/if}
 </main>
 
 <style>
   main {
     text-align: center;
     padding: 1em;
-    max-width: 450px;
+    max-width: 600px;
     margin: 0 auto;
   }
 </style>
